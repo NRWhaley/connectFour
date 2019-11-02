6aa2c1346cf1612a6e9dd7ccf5bc7dd1ed889265
Nick Whaley <nicholasrwhaley@gmail.com>
2019-11-01 19:54:27 -0700

    refactor based on codepen and consolidate

--- connectfour/src/App.js
+++ connectfour/src/App.js
@@ -23,62 +23,177 @@ const boardStyle = {
 
 class App extends React.Component {
   constructor(props){
-    super(props)
-    this.state = {
-      turn: 'red'
+    super(props);
+    this.state={
+      player1: 1,
+      player2: 2,
+      currentPlayer: null,
+      board: [],
+      gameOver: false,
+      message: ''
     }
-
-    this.boardClick = this.boardClick.bind(this)
-
+    this.play = this.play.bind(this)
   }
 
-  boardClick() {
-    if(this.state.turn === 'red'){
-      this.setState({
-        turn: 'blue'
-      })
-      console.log('blue')
+  initBoard() {
+    let board = [];
+    for(let r=0; r < 6; r++) {
+      let row = [];
+      for(let c = 0; c < 7; c++){
+        row.push(null)
+      }
+      board.push(row)
+    }
+    this.setState({
+      board,
+      currentPlayer: this.state.player1,
+      gameOver: false,
+      message: '',
+
+    })
+  }
+
+  togglePlayer() {
+    return (this.state.currentPlayer === this.state.player1) ? this.state.player2 : this.state.player1;
+  }
+
+  play(c) {
+    if(!this.state.gameOver){
+      let board = this.state.board;
+      for(let r = 5; r >= 0; r--) {
+        if(!board[r][c]) {
+          board[r][c] = this.state.currentPlayer;
+          break;
+        }
+      }
+
+      let result = this.checkAll(board);
+      if(result === this.state.player1){
+        this.setState({board, gameOver: true, message: 'Red wins!'})
+      } else if (result === this.state.player2){
+        this.setState({board, gameOver: true, message: 'Yello wins!'})
+      } else if(result === 'draw'){
+        this.setState({
+          board, gameOver: true, message: 'Draw'
+        })
+      } else {
+        this.setState({ board, currentPlayer: this.togglePlayer()})
+      }
     } else {
-      this.setState({
-        turn: 'red'
-      })
-      console.log('red')
+      this.setState({message: 'Game Over. Play again?'})
     }
+
   }
 
+    checkVertical(board) {
+      for(let r = 3; r < 6; r++) {
+        for(let c = 0; c < 7; c++){
+          if(board[r][c]){
+            if(board[r][c] === board[r - 1][c] &&
+              board[r][c] === board[r - 2][c] &&
+              board[r][c] === board[r - 3][c]) {
+                return board[r][c]
+              }
+          }
+        }
+      }
+    }
 
-  render() {
+    checkHorizontal(board) {
+      for(let r = 0; r < 6; r++){
+        for(let c = 0; c < 4; c++) {
+          if(board[r][c]) {
+            if(board[r][c] === board[r][c + 1] &&
+              board[r][c] === board[r][c + 2] &&
+              board[r][c] === board[r][c + 3]) {
+                return board[r][c]
+              }
+          }
+        }
+      }
+    }
+
+    checkDiagonalRight(board) {
+      for(let r = 3; r < 6; r++){
+        for(let c = 0; c < 4; c++){
+          if(board[r][c]){
+            if(board[r][c] === board[r - 1][c +1] &&
+              board[r][c] === board[r - 2][r + 2] &&
+              board[r][c] === board[r - 3][c + 3]) {
+                return board[r][c]
+              }
+          }
+        }
+      }
+    }
+
+    checkDraw(board) {
+      for(let r = 0; r < 6; r++){
+        for (let c = 0; c < 7; c++){
+          if(board[r][c] === null) {
+            return null
+          }
+        }
+      }
+      return 'draw'
+    }
+
+    checkAll(board) {
+      return this.checkVertical(board) || this.checkDiagonalRight(board) || this.checkDiagonalLeft(board) || this.checkHorizontal(board) || this.checkDraw(board)
+    }
+
+    componentWillMount() {
+      this.initBoard();
+    }
+
+    render() {
+      return (
+        <div>
+          <div className="button" onClick={() => {this.initBoard()}}>New Game</div>
+
+          <table>
+            <thead>
+            </thead>
+            <tbody>
+              {this.state.board.map((row, i) => (<Row key={i} row={row} play={this.play} />))}
+            </tbody>
+          </table>
+
+          <p className="message">{this.state.message}</p>
+        </div>
+      );
+    }
+
+
+
+}
+
+const Row = ({ row, play }) => {
+  return (
+    <tr>
+      {row.map((cell, i) => <Cell key={i} value={cell} columnIndex={i} play={play} />)}
+    </tr>
+  );
+};
+
+const Cell = ({ value, columnIndex, play }) => {
+  let color = 'white';
+  if (value === 1) {
+    color = 'red';
+  } else if (value === 2) {
+    color = 'yellow';
+  }
 
   return (
-    <div className='Head' style={headStyle}>
-      <Game />
+    <td>
+      <div className="cell" onClick={() => {play(columnIndex)}}>
+        <div className={color}></div>
+      </div>
+    </td>
+  );
+};
 
-    <div className="App" style={boardStyle}>
-      <div>
-      <Column row={0} turn={this.state.turn}/>
-      </div>
-      <div>
-      <Column row={1} turn={this.state.turn}/>
-      </div>
-      <div>
-      <Column row={2} turn={this.state.turn}/>
-      </div>
-      <div>
-      <Column row={3} turn={this.state.turn}/>
-      </div>
-      <div>
-      <Column row={4} turn={this.state.turn}/>
-      </div>
-      <div>
-      <Column row={5} turn={this.state.turn}/>
-      </div>
-      <div>
-      <Column row={6} turn={this.state.turn}/>
-      </div>
 
-    </div>
-  </div>
-  )};
-}
+
 
 export default App;
